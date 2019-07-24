e32c7f2b23543f676a6109c4396f253a1ab427ab
linhphamdinh <53222744+linhphamdinh@users.noreply.github.com>
2019-07-25 05:47:11 +0700

    Add files via upload

---
+++ nhap.py
@@ -0,0 +1,10 @@
+# bieu thuc dk chi tra ve dung hoac sai
+# block khoi code dc nhan biet bang do thut dong   
+# phan luong : chay bang f5 nhan f10 de xem chay tung dong code
+#              xem cac gia tri bien voi muc whatch
+# range([star],stop,[step])
+# range(6) = range(1,7,1)
+# pinrt(x,end=" ")
+# a = 4%2 == 0
+# print(a)
+# ctrl+/ (chu thich tat ca dong boi den)
